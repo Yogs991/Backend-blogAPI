8566ad7c509dd1dfa7827cfa52cca0e4746ef0d0
Yog99 <124916155+Yogs991@users.noreply.github.com>
2026-03-29 14:11:55 +0300

    Changes to comment section

--- routes/commentRouter.js
+++ routes/commentRouter.js
@@ -7,9 +7,9 @@ const validator = require("../utils/validators");
 
 // => /api/comments/postId/comments
 commentRouter.get("/:postId/comments", commentController.getCommentsForPost);
-commentRouter.get("/:postId/:commentId", commentController.getCommentById);
+commentRouter.get("/:postId/comments/:commentId", commentController.getCommentById);
 commentRouter.post("/:postId/comments", validator.createCommentValidation, commentController.addComment);
-commentRouter.put("/:postId/:commentId", validator.updateCommentValidation, commentController.updateComment);
-commentRouter.delete("/:postId/:commentId", commentController.deleteComment);
+commentRouter.put("/:postId/comments/:commentId", validator.updateCommentValidation, commentController.updateComment);
+commentRouter.delete("/:postId/comments/:commentId", commentController.deleteComment);
 
 module.exports = commentRouter;
